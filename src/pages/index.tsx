3468531039f93cf28d7458b80545c166acbf8a73
Pablo Sáez Parra <pablosaez1995@gmail.com>
2021-08-06 18:58:07 -0400

    use node 14

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Stack, Text } from '@chakra-ui/react';
-import { useQuery } from '../gqty';
+import { Stack, Text } from "@chakra-ui/react";
+import { useQuery } from "../gqty";
 
 export default function Home() {
   const { hello } = useQuery();
   return (
     <Stack>
-      <Text fontSize="3em">zxczxc World</Text>
-      <Text>{hello || 'Loadiasdasdng...'}</Text>
+      <Text fontSize="3em">Hello World</Text>
+      <Text>{hello || "Loading..."}</Text>
     </Stack>
   );
 }
