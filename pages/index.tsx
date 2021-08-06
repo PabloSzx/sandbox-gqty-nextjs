a2a83a5ba19e547737cf7652994a164f9fe6bf80
PabloSzx <pablosaez1995@gmail.com>
2021-08-06 18:01:29 -0400

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   const { hello } = useQuery();
   return (
     <Stack>
-      <Text fontSize="3em">Hello World</Text>
-      <Text>{hello || 'Loading...'}</Text>
+      <Text fontSize="3em">Hellozxc World</Text>
+      <Text>{hello || 'Loadiasdasdng...'}</Text>
     </Stack>
   );
 }
