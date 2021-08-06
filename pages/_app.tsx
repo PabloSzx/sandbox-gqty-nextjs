9a625b13530470206e75f3865991bcf680b959d1
PabloSzx <pablosaez1995@gmail.com>
2021-08-06 17:46:57 -0400

    2 files modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { ChakraProvider } from "@chakra-ui/react";
 
 import type { AppProps } from "next/app";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import { Stack, Text } from "@chakra-ui/react";
+import * as React from 'react';
+
+import { Stack, Text } from '@chakra-ui/react';
+import { useQuery } from '../src/gqty';
 
 export default function Home() {
+  const { hello } = useQuery();
   return (
     <Stack>
       <Text fontSize="3em">Hello World</Text>
+      <Text>{hello || 'Loading...'}</Text>
     </Stack>
   );
 }
